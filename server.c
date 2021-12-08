ab128965f49d451245ba5a43d5c0c0145b5b36af
yeeun <musiclove529@naver.com>
2021-12-08 23:50:07 +0900

    assign locker

--- server.c
+++ server.c
@@ -19,8 +19,8 @@ int main(int argc, char *argv[]){
 	int lockerNum, bigNum, pwdLen;
 	struct student record;
 	struct locker * locker;
-	int id,menu;
-	char outmsg[MAXLINE], inmsg[MAXLINE], name[20];
+	int id,menu,lockerId;
+	char outmsg[MAXLINE], inmsg[MAXLINE];
 
 	signal(SIGCHLD, SIG_IGN);
 	clientlen = sizeof(clientUNIXaddr);
@@ -41,14 +41,15 @@ int main(int argc, char *argv[]){
 
 	locker = (struct locker *)malloc(lockerNum * sizeof(struct locker));
 	for(int i = 0;i<lockerNum;i++){
-		locker[i].id = i;
-		if(i<bigNum){
+		locker[i].id = i+1;
+		if(i<lockerNum-bigNum){
 			locker[i].isBig = 0;
 			locker[i].cap = 5;
 		}else{
 			locker[i].isBig = 1;
 			locker[i].cap = 10;
 		}
+		locker[i].pwd = (char*)malloc(pwdLen*sizeof(char));
 		locker[i].wrongCnt = 0;
 		locker[i].islock = 0;
 		locker[i].lockTime = 0;
@@ -59,9 +60,9 @@ int main(int argc, char *argv[]){
 	while(1){
 		connfd = accept(listenfd, &clientUNIXaddr, &clientlen);
 		if(fork() == 0){
-			if((fd = open(argv[1],O_RDWR|O_CREAT|O_TRUNC,0640))==-1){
-				perror(argv[1]);
-				exit(2);
+			if((fd = open(argv[1], O_RDWR|O_CREAT, 0640)) == -1){
+                        	perror(argv[1]);
+                        	exit(2);
 			}
 			writeInfo(connfd, "학번 : ", inmsg, 1);
 			id = atoi(inmsg);
@@ -69,17 +70,23 @@ int main(int argc, char *argv[]){
 				printf("wrong id");
 				exit(2);
 			}
-			lseek(fd,(long)(id-START_ID)*sizeof(record),SEEK_SET);
-			if((read(fd,&record,sizeof(record))<0)||(record.id==0)){
+			printf("id : %d\n", id);
+			lseek(fd,(id-START_ID)*sizeof(record),SEEK_SET);
+			int n = read(fd, &record, sizeof(record));
+			printf("n : %d\n", n);
+			if(n<=0||record.id==0){
+				printf("norecord\n");
 				record.id = id;
-				writeInfo(connfd, "이름 : ", name, 1);
-				strcpy(record.name, name);
-				lseek(fd,-sizeof(record),SEEK_CUR);
+				writeInfo(connfd, "이름 : ", inmsg, 1);
+				strncpy(record.name, inmsg, 20);
+				lseek(fd,(record.id-START_ID)*sizeof(record),SEEK_SET);
 				write(fd,&record,sizeof(record));
+			}else{
+				printf("record : %d\t %s\n", record.id,record.name);
 			}
-			printf("%s님이 로그인했습니다\n",name);
+			printf("%s님이 로그인했습니다\n",record.name);
 			while(1){
-				sprintf(outmsg, "%s\n -----메뉴-----\n 1. 사물함 신청\n 2. 내 사물함 보기\n 3. 종료\n",name);
+				sprintf(outmsg, "-----메뉴-----\n 1. 사물함 신청\n 2. 내 사물함 보기\n 3. 종료\n");
 				writeInfo(connfd, outmsg, inmsg, 1);
 				menu = atoi(inmsg);
 				if(menu == 1){
@@ -88,15 +95,41 @@ int main(int argc, char *argv[]){
 						sprintf(outmsg,"id : %d, is Big : %d, cap : %d\n", locker[i].id, locker[i].isBig, locker[i].cap);
 						writeInfo(connfd, outmsg, inmsg,0);
 					}
+					writeInfo(connfd, "사물함 선택 : ", inmsg, 1);
+					lockerId = atoi(inmsg) - 1;
+					do{
+						writeInfo(connfd, "비밀번호 입력 : ", inmsg, 1);
+						if(strlen(inmsg)!=pwdLen){
+						       	sprintf(outmsg, "비밀번호는 %d자리여야 합니다.\n",pwdLen);
+							writeInfo(connfd, outmsg, inmsg, 0);
+						}
+
+					}while(strlen(inmsg)!=pwdLen);
+					strncpy(locker[lockerId].pwd, inmsg,pwdLen);
+					record.myLocker = locker[lockerId];
+					lseek(fd,-sizeof(record),SEEK_CUR);
+					write(fd,&record,sizeof(record));
+					printf("%d\t %s\t %d\t %s\n",record.id, record.name, record.myLocker.id, record.myLocker.pwd);
 				}else if (menu == 2){
 					printf("내 사물함 보기\n");
+					writeInfo(connfd, "비밀번호 : ", inmsg, 1);
+					if(strcmp(record.myLocker.pwd, inmsg)==0){
+						sprintf(outmsg, "사물함 번호 : %d\t 남은 공간 : %d\n", record.myLocker.id, record.myLocker.cap);
+						writeInfo(connfd, outmsg, inmsg, 0);
+
+					}else{
+						writeInfo(connfd, "비밀번호가 맞지않습니다.\n", inmsg, 0);
+					}
 				}else if(menu == 3){
-					printf("%s님이 로그아웃했습니다\n", name);
+					printf("%s님이 로그아웃했습니다\n", record.name);
+					close(fd);
 					exit(0);
 				}
 			}
 
-		}else close(connfd);
+		}else {
+			close(connfd);
+		}
 	}
 }
 
